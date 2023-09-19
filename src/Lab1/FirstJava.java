f596ddb2be86bc461bc70f7fed01bce0c06006a2
Daniel <marchisdaniel31@gmail.com>
2023-09-19 23:37:17 +0300

    lab1

---
+++ src/Lab1/FirstJava.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+
+}
+
+// PascalCase - un case in care prima litera din fiecare cuvant e mare, iar restul sunt mici(numele claselor)
+// camelCase - un case in care prima litera din fiecare cuvant, incepand cu al 2-lea e mare, iar restul sunt mici(numele metodelor/functiilor)
+// snake_case/SNAKE_CASE - cuvintele sunt despartite de "_"
+// kebab-case/KEBAB-CASE - cuvintele sunt despartite de "-"
---
+++ src/Lab1/Homework.java
@@ -0,0 +1,7 @@
+package Lab1;
+
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("My first homework.");
+    }
+}
